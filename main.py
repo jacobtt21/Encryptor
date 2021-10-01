65145fb88a70e3a2a147640f7df126f0876323ac
Jacob Thomas <jacobthomas@Jacobs-MacBook-Pro-3.local>
2021-10-01 00:28:34 -0500

    added password feature

--- main.py
+++ main.py
@@ -8,20 +8,28 @@ load_dotenv()
 app = Flask(__name__)
 
 key = os.getenv('KEY_VALUE')
+password = os.getenv('PASS_VALUE')
 key.encode()
 fernet = Fernet(key)
 
 @app.errorhandler(500)
 def our_bad(e):
-    return render_template('index.html', message="Something is wrong, slack Jacob if something is wrong"), 500
+    return render_template('index.html', message="Ensure that you are inputting the correct input!"), 500
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/enc/', defaults={'enc': None})
-@app.route('/enc/<enc>')
-def find(enc):
+@app.route('/enc/', defaults={'enc': None, 'passw': None})
+@app.route('/enc/<enc>/<passw>')
+def find(enc, passw):
+    passes = request.args.get('passw')
     encs = request.args.get('enc')
-    encs.encode()
-    return render_template('index.html', message = fernet.decrypt(encs.encode()).decode())
+    encs = encs.encode()
+    if passes == password:
+        return render_template('index.html', message = fernet.decrypt(encs).decode())
+    else:
+        return render_template('index.html', message = "incorrect password")
+
+if __name__ == '__main__':
+    app.run(debug=True)
