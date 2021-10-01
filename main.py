f7cd5bcb12ae9b3507b30586296516338928fed6
Jacob Thomas <jacobthomas@Jacobs-MacBook-Pro-3.local>
2021-10-01 00:41:46 -0500

    added POST requests inseatd of args

--- main.py
+++ main.py
@@ -16,20 +16,28 @@ fernet = Fernet(key)
 def our_bad(e):
     return render_template('index.html', message="Ensure that you are inputting the correct input!"), 500
 
+@app.errorhandler(405)
+def our_bad(e):
+    return render_template('index.html', message="Cannot refresh page, security reasons"), 405
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/enc/', defaults={'enc': None, 'passw': None})
-@app.route('/enc/<enc>/<passw>')
-def find(enc, passw):
-    passes = request.args.get('passw')
-    encs = request.args.get('enc')
-    encs = encs.encode()
-    if passes == password:
-        return render_template('index.html', message = fernet.decrypt(encs).decode())
-    else:
-        return render_template('index.html', message = "incorrect password")
+@app.route('/enc', methods=['POST'])
+def find():
+    if request.method == 'POST':
+        passes = request.form['passw']
+        encs = request.form['enc']
+        encs = encs.encode()
+        if passes == password:
+            return render_template('index.html', message = fernet.decrypt(encs).decode())
+        else:
+            return render_template('index.html', message = "incorrect password")
+
+##Test Cases
+##gAAAAABhVp4UJe0o1706MiFXuQnJxqIlWAPlv8j0qGZSi_CRiGZtNnPmmVNUwXG97TZ1yTTOwpauL3ZIPhSNIMWL0oqXvC3r5VsnGFMywrimKUkKZ8SHtrY=
+##gAAAAABhVp4s2uzu2p_jh5V0HRLDrlnh2jRLcWTJWTTSB-YlZ984y9jGzwZeyFUOFIDmgy3nKjvUwLhTIk2E9EmN2qZOZupidxrO6s7Y-oeaFlBrqPvadJ4=
 
 if __name__ == '__main__':
     app.run(debug=True)
